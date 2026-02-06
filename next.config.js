dea7d1f21b9b62e0e0fa93cf6fd7f66bf261f9d8
Daniel-ths <danielvieitas2004@gmail.com>
2026-02-05 22:55:01 -0300

    se deus quiser ultimas att

--- next.config.js
+++ next.config.js
@@ -3,11 +3,8 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     remotePatterns: [
-      // Seus domínios atuais
       { protocol: 'https', hostname: 'qkpfsisyaohpdetyhtjd.supabase.co' },
       { protocol: 'https', hostname: 'placehold.co' },
-      
-      // Novos domínios necessários para o carrossel
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'production.autoforce.com' },
     ],
